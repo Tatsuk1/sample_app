6e4507cb9b10f1fda232d2f40ba6e84e0d4a67e9
EC2 Default User <ec2-user@ip-172-31-92-113.ec2.internal>
2019-08-24 02:54:55 +0000

    Add account activation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -74,6 +74,6 @@ class UserTest < ActiveSupport::TestCase
   end
   
   test"suthenticated? should return false for a user with nil digest" do
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
   end
 end
